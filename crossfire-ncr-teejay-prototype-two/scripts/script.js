183ac91598cc9d446fe83de2d8fa6979a0b5ae25
Teejay Madlangbayan <40991102+BennyTejadaMaglaya@users.noreply.github.com>
2024-11-15 10:10:11 -0500

    Updates to Page Navigation and Content Loading
    	Added view, ncr-form, and faqs pages.
    	Added loadPage function to dynamically load pages into the main content area without redirecting.

--- crossfire-ncr-teejay-prototype-two/scripts/script.js
+++ crossfire-ncr-teejay-prototype-two/scripts/script.js
@@ -40,3 +40,53 @@ modeSwitch.addEventListener("click", () => {
     modeText.innerText = "Dark mode";
   }
 });
+
+/*Load different pages into the main content area*/
+function loadPage(page) {
+  const contentArea = document.getElementById('mainContent');
+  // const ncrTable = document.getElementById("ncrTable");
+  const pageTitle = document.querySelector('.homeSection #pageTitle h1');
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', page, true);
+
+  xhr.onload = async function () {
+    if (this.status === 200) {
+      contentArea.innerHTML = this.responseText;
+
+      // ncrTable.style.display = "none";
+
+      switch (page) {
+        case 'index.html':
+          pageTitle.textContent = 'Dashboard';
+          break;
+
+        case 'view.html':
+          pageTitle.textContent = 'View NCRs';
+          break;
+
+        case 'ncr-form.html':
+          pageTitle.textContent = 'NCR Form';
+          // document.getElementById('ncrNo').value = await generateNCRNumber();
+          // document.getElementById('processApplicable1').focus();
+          break;
+
+        case 'faqs.html':
+          pageTitle.textContent = 'FAQs';
+          break;
+
+        default:
+          pageTitle.textContent = 'Dashboard';
+          break;
+      }
+    } else {
+      contentArea.innerHTML = '<p>&emsp;Failed to load content.</p>';
+    }
+  };
+
+  xhr.onerror = function () {
+    contentArea.innerHTML = '<p>&emsp;There was an error loading the page.</p>';
+  };
+
+  xhr.send();
+}
